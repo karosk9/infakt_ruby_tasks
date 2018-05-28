0e55fab6a3ec0e1ec6b53ead35aaacadaf3b6798
Karolina <karosk9@gmail.com>
2018-05-28 09:32:57 +0200

    resolve passed or failed task

--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,23 @@
 class PassedOrFailed
 
+  def initialize(students, pass_limit)
+    @students = students
+    @pass_limit = pass_limit.to_i
+  end
+
+  def perform
+    results = {
+      :failed => {},
+      :passed => {}
+    }
+    @students.each do |student_name, score|
+      score = score.to_i
+      if score < @pass_limit
+        results[:failed][student_name] = score
+      else
+        results[:passed][student_name] = score
+      end
+    end
+    results
+  end
 end

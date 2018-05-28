ed8de1ef42963f6f35b8c48d3593ba8f5251931f
Karolina <karosk9@gmail.com>
2018-05-28 09:32:26 +0200

    resolve caesar cifer task

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,21 @@
-class CeasarCipher
+class CaesarCipher
 
+  def initialize(text, displacement = 13)
+    @text = text
+    @displacement = displacement
+  end
+
+  def perform
+    alphabet = ('a'..'z').to_a
+    ciphred_text = @text.split('').map do |letter|
+      if alphabet.include?(letter.downcase) == false
+        letter
+      else
+        letter_index = alphabet.index(letter.downcase)+ @displacement
+        letter_index = letter_index - alphabet.length if letter_index >= alphabet.length
+        letter.downcase! != nil ? letter = alphabet[letter_index].upcase : letter = alphabet[letter_index]
+      end
+    end.join
+    ciphred_text
+  end
 end
